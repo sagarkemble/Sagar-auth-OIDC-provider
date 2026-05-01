51f09b7224d0ccb7f3fd77bd83f38eee1e620e95
Sagar Kemble <sagarkemble.sasa@gmail.com>
2026-05-01 18:12:55 +0530

    feat: add userCodes table

---
+++ src/modules/auth/models/auth.userCodes.mode.ts
@@ -0,0 +1,28 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  text,
+  boolean,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import clientTable from "../../client/client.model";
+import usersTable from "./auth.users.model";
+
+const userCodesTable = pgTable("user_codes", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  clientId: uuid("client_id")
+    .notNull()
+    .references(() => clientTable.id),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => usersTable.id),
+  refreshToken: text("refresh_token"),
+  authorizationCode: text("authorization_code"),
+  refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+  authorizationCodeExpiresAt: timestamp("authorization_code_expires_at"),
+  updatedAt: timestamp("updated_at"),
+  createdAt: timestamp("created_at"),
+});
+
+export default userCodesTable;
